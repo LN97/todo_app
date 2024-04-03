2bfb53a333db01ef3c7f0447e94804141dacc1f2
LN97 <rolisnini@gmail.com>
2024-04-03 18:48:15 +0100

    added cloudinary preset

--- src/cloudinary.js
+++ src/cloudinary.js
@@ -1,11 +1,35 @@
+// const cloudinary = require('cloudinary').v2;
+
+// // Your existing configuration...
+
+// async function uploadImage(imageBase64) {
+//     try {
+//         // Uploading the image
+//         const result = await cloudinary.uploader.upload(imageBase64);
+        
+//         // Returning the URL of the uploaded image
+//         return result.url;
+
+//     } catch (error) {
+//         console.error('Upload to Cloudinary failed:', error);
+//         throw error;
+//     }
+// }
+
+// module.exports = {
+//     uploadImage
+// };
+
 const cloudinary = require('cloudinary').v2;
 
 // Your existing configuration...
 
 async function uploadImage(imageBase64) {
     try {
-        // Uploading the image
-        const result = await cloudinary.uploader.upload(imageBase64);
+        // Uploading the image with an upload preset
+        const result = await cloudinary.uploader.upload(imageBase64, {
+            upload_preset: 'k4kqkqbu'
+        });
         
         // Returning the URL of the uploaded image
         return result.url;
