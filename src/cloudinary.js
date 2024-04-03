51752af6dc49fa9c26d396b44086f8007f4e507b
LN97 <rolisnini@gmail.com>
2024-04-03 18:13:45 +0100

    ...

--- src/cloudinary.js
+++ src/cloudinary.js
@@ -4,26 +4,8 @@ const cloudinary = require('cloudinary').v2;
 
 async function uploadImage(imageBase64) {
     try {
-        const timestamp = Math.round((new Date()).getTime() / 1000);
-
-        // Use Cloudinary's API to generate the signature
-        const signaturePayload = {
-            timestamp: timestamp,
-            // Include any other parameters you want to send for the upload
-        };
-
-        // Generating the signature
-        const signatureResult = cloudinary.utils.api_sign_request(signaturePayload, process.env.CLOUDINARY_API_SECRET);
-
-        // Constructing parameters for the upload, including the generated signature
-        const params = {
-            ...signaturePayload,
-            api_key: process.env.CLOUDINARY_API_KEY,
-            signature: signatureResult
-        };
-
         // Uploading the image
-        const result = await cloudinary.uploader.upload(imageBase64, params);
+        const result = await cloudinary.uploader.upload(imageBase64);
         
         // Returning the URL of the uploaded image
         return result.url;
