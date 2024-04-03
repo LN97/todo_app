4772682556b334ff546a1097da65e624417e8e3a
LN97 <rolisnini@gmail.com>
2024-04-03 17:56:55 +0100

    test

--- src/cloudinary.js
+++ src/cloudinary.js
@@ -1,5 +1,4 @@
 const cloudinary = require('cloudinary').v2;
-const sha1 = require('sha1');
 
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
@@ -9,23 +8,8 @@ cloudinary.config({
 
 async function uploadImage(imageBase64) {
     try {
-        // Ensure your timestamp is consistent across signature generation and the parameters
-        const timestamp = Math.round((new Date()).getTime() / 1000);
-
-        // Generate the signature using the sorted parameter string
-        const paramsToSign = `folder=todoapp&timestamp=${timestamp}${cloudinary.config().api_secret}`;
-        const signature = sha1(paramsToSign);
-
-        // Construct the full set of parameters for the upload
-        const params = {
-            timestamp,
-            folder: 'todoapp',
-            signature,
-            api_key: process.env.CLOUDINARY_API_KEY // Include the API Key here as well
-        };
-
         // Directly await the promise returned by the upload method
-        const result = await cloudinary.uploader.upload(imageBase64, params);
+        const result = await cloudinary.uploader.upload(imageBase64);
 
         // Once the upload is successful, return the URL
         return result.url;
